990a00a82fae09955d801cdb045218c4d54e1fc1
haz3141 <38766403+haz3141@users.noreply.github.com>
2023-11-13 18:51:46 -0500

    Updated models

--- server/models/eventModel.js
+++ server/models/eventModel.js
@@ -5,10 +5,29 @@ const eventSchema = new mongoose.Schema({
   description: String,
   type: { type: String, required: true }, // e.g., matchup, contest, competition, wager
   entryFee: { type: Number, required: true },
+  maxParticipants: { type: Number, required: true }, // Max number of participants
   startTime: { type: Date, required: true },
   endTime: Date,
-  participants: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }], // Assuming you have a User model
-  // You can add additional fields as needed
+  status: {
+    type: String,
+    enum: [
+      "awaitingDeposit",
+      "awaitingParticipants",
+      "active",
+      "finished",
+      "settled",
+      "cancelled",
+    ],
+    default: "awaitingDeposit",
+  },
+  // Store participant addresses since we are not using user accounts
+  participants: [{ type: String }],
+  // Additional fields to manage Bitcoin transactions, like addresses or transaction IDs
+  transactionDetails: {
+    creatorDeposit: { type: String }, // Bitcoin transaction ID for creator's deposit
+    participantDeposits: [{ type: String }], // Bitcoin transaction IDs for participants' deposits
+  },
+  // Additional fields as needed...
 });
 
 const Event = mongoose.model("Event", eventSchema);
--- server/models/walletModel.js
+++ server/models/walletModel.js
@@ -2,9 +2,11 @@ const mongoose = require("mongoose");
 
 const walletSchema = new mongoose.Schema({
   address: { type: String, required: true, unique: true },
-  privateKey: { type: String, required: true },
-  // You can add additional fields as needed, such as userId if wallets are tied to specific users
-  // userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+  privateKey: {
+    iv: { type: String, required: true },
+    content: { type: String, required: true },
+  },
+  // Add any additional fields as needed
 });
 
 const Wallet = mongoose.model("Wallet", walletSchema);
