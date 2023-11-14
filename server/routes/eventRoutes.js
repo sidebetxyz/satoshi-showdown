5f288dc535b7ce88c2afc66fed76efc67d9165ae
haz3141 <38766403+haz3141@users.noreply.github.com>
2023-11-14 17:11:08 -0500

    Updated event service and routes

--- server/routes/eventRoutes.js
+++ server/routes/eventRoutes.js
@@ -2,14 +2,18 @@ const express = require("express");
 const router = express.Router();
 const eventService = require("../services/eventService");
 
+// Route to create a new event
 router.post("/create", async (req, res) => {
   try {
-    const newEvent = await eventService.createEvent(req.body);
+    const eventData = req.body;
+    const newEvent = await eventService.createEvent(eventData);
     res.status(201).json(newEvent);
   } catch (error) {
     console.error("Error creating event:", error);
     res.status(500).send("Error creating event");
   }
 });
+
+// Additional event-related routes can be added here as needed
 
 module.exports = router;
--- server/services/eventService.js
+++ server/services/eventService.js
@@ -1,16 +1,37 @@
-const Event = require("../models/eventModel"); // Adjust the path as needed
-const { createSegwitWallet } = require("./walletService"); // For generating wallet addresses
+const Event = require("../models/eventModel");
+const Blockchain = require("../models/blockchainModel");
+const walletService = require("./walletService"); // Assuming this service has the wallet creation logic
 
 const eventService = {
   async createEvent(eventData) {
-    // Generate a Bitcoin address for the event creator
-    const creatorWallet = await createSegwitWallet();
-    eventData.creatorDepositAddress = creatorWallet.address;
+    try {
+      // Generate a Bitcoin address for the event using the wallet service
+      const creatorWallet = await walletService.createSegwitWallet();
 
-    // Create the event
-    const newEvent = new Event(eventData);
-    await newEvent.save();
-    return newEvent;
+      // Add wallet reference to the event data
+      eventData.wallet = creatorWallet._id;
+
+      // Create the event
+      const newEvent = new Event({
+        ...eventData,
+        // Add other event-specific data
+      });
+      await newEvent.save();
+
+      // Create an initial blockchain entry for the event
+      const newBlockchainEntry = new Blockchain({
+        event: newEvent._id,
+        address: creatorWallet.address,
+        transactionStatus: "waiting", // Initial status
+        // other blockchain fields
+      });
+      await newBlockchainEntry.save();
+
+      return newEvent;
+    } catch (error) {
+      console.error("Error creating event:", error);
+      throw error;
+    }
   },
 };
 
