65286645c28d99296a9f8e589f734141391ad7ff
Pi <38766403+haz3141@users.noreply.github.com>
2023-12-13 14:07:12 -0500

    Added health check route

--- server/src/utils/routeUtil.js
+++ server/src/utils/routeUtil.js
@@ -1,14 +1,19 @@
 /**
  * @fileoverview Route Utility for Satoshi Showdown.
- * This module provides functionality for consolidating and applying route handling
- * in the Express application. It imports route modules for different application
- * functionalities (like users, events, and webhooks) and applies them to the Express application,
- * organizing the routing structure in a central location.
+ * This module serves as a centralized point for consolidating and applying route handling
+ * within the Express application. It is responsible for importing route modules that cater to
+ * different functional aspects of the application, such as user management, event handling,
+ * and webhook processing. This organization facilitates a modular and maintainable routing structure,
+ * ensuring that application routes are systematically structured and easily scalable.
+ *
+ * This module also includes a health check endpoint for monitoring the application's operational status,
+ * which is crucial for ensuring the reliability and availability of the service.
  *
  * @module utils/routeUtil
- * @requires routes/userRoutes - Routing module for user-related endpoints.
- * @requires routes/eventRoutes - Routing module for event-related endpoints.
- * @requires routes/webhookRoutes - Routing module for webhook endpoints.
+ * @requires express.Application - Express application to which the routes are applied.
+ * @requires routes/userRoutes - Routing module for handling user-related functionalities.
+ * @requires routes/eventRoutes - Routing module for managing event-related operations.
+ * @requires routes/webhookRoutes - Routing module for processing webhook events.
  */
 
 const userRoutes = require("../routes/userRoutes");
@@ -16,17 +21,32 @@ const eventRoutes = require("../routes/eventRoutes");
 const webhookRoutes = require("../routes/webhookRoutes");
 
 /**
- * Applies a predefined set of route modules to an Express application.
- * This function simplifies the process of adding routes to the app,
- * ensuring that they are added in a consistent and organized manner.
+ * Integrates various route modules into the Express application.
+ * This function systematically adds each group of route handlers to the application instance,
+ * segregating them based on their functional domain. Additionally, it establishes a health check
+ * endpoint, allowing for routine health and performance monitoring of the application.
+ *
+ * The health check endpoint is a lightweight and responsive route that returns a standard
+ * success response, signifying the operational status of the application.
  *
  * @function applyRoutes
  * @param {express.Application} app - The Express application to which routes will be applied.
  */
 const applyRoutes = (app) => {
+  // User-related routes
   app.use("/user", userRoutes);
+
+  // Event-related routes
   app.use("/event", eventRoutes);
+
+  // Webhook processing routes
   app.use("/webhook", webhookRoutes);
+
+  // Health Check Route
+  app.get("/health", (req, res) => {
+    // Respond with OK status to indicate that the application is running properly
+    res.status(200).send("OK");
+  });
 };
 
 module.exports = { applyRoutes };
